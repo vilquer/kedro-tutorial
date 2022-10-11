ed58f1fc6133aa5143159f58045d364d208c7dfb
vilquer <vilquer@gmail.com>
2022-10-10 21:36:41 -0300

    organização do projeto em namespaces, divisão do pipiline em dois fluxos

--- src/kedro_tutorial/pipelines/data_processing/pipeline.py
+++ src/kedro_tutorial/pipelines/data_processing/pipeline.py
@@ -2,10 +2,20 @@
 This is a boilerplate pipeline 'data_processing'
 generated using Kedro 0.18.3
 """
+import pandas as pd
+import plotly.express as px
 
-from kedro.pipeline import Pipeline, node, pipeline
+from kedro.pipeline import Pipeline, node
+from kedro.pipeline.modular_pipeline import pipeline
 
-from .nodes import create_model_input_table, preprocess_companies, preprocess_shuttles
+from .nodes import (
+    preprocess_companies,
+    preprocess_shuttles,
+    create_model_input_table,
+)
+
+def compare_passenger_capacity(preprocessed_shuttles: pd.DataFrame):
+    return preprocessed_shuttles.groupby(["shuttle_type"]).mean().reset_index()
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
@@ -28,5 +38,13 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs="model_input_table",
                 name="create_model_input_table_node",
             ),
-        ]
+            node(
+                func=compare_passenger_capacity,
+                inputs="preprocessed_shuttles",
+                outputs="shuttle_passenger_capacity_plot",
+            ),
+        ],
+        namespace="data_processing",
+        inputs=["companies", "shuttles", "reviews"],
+        outputs=["model_input_table", "shuttle_passenger_capacity_plot"],
     )
--- src/kedro_tutorial/pipelines/data_science/pipeline.py
+++ src/kedro_tutorial/pipelines/data_science/pipeline.py
@@ -2,20 +2,15 @@
 This is a boilerplate pipeline 'data_science'
 generated using Kedro 0.18.3
 """
-import pandas as pd
-import plotly.express as px
 
-from kedro.pipeline import Pipeline, node, pipeline
+from kedro.pipeline import Pipeline, node
+from kedro.pipeline.modular_pipeline import pipeline
 
 from .nodes import evaluate_model, split_data, train_model
 
 
-def compare_passenger_capacity(preprocessed_shuttles: pd.DataFrame):
-    return preprocessed_shuttles.groupby(["shuttle_type"]).mean().reset_index()
-
-
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline(
+    pipeline_instance = pipeline(
         [
             node(
                 func=split_data,
@@ -35,10 +30,21 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs=None,
                 name="evaluate_model_node",
             ),
-            node(
-                func=compare_passenger_capacity,
-                inputs="preprocessed_shuttles",
-                outputs="shuttle_passenger_capacity_plot",
-            ),
         ]
+    )
+    ds_pipeline_1 = pipeline(
+        pipe=pipeline_instance,
+        inputs="model_input_table",
+        namespace="active_modelling_pipeline",
+    )
+    ds_pipeline_2 = pipeline(
+        pipe=pipeline_instance,
+        inputs="model_input_table",
+        namespace="candidate_modelling_pipeline",
+    )
+
+    return pipeline(
+    pipe=ds_pipeline_1 + ds_pipeline_2,
+    inputs="model_input_table",
+    namespace="data_science",
     )
